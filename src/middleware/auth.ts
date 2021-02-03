94b804fd474bf697b3601a5a2d32c6d4655839fa
Lagoons Browning <lagoons@protonmail.com>
2021-02-02 23:34:01 -0600

    cors again

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,7 +1,5 @@
 import {verify} from 'jsonwebtoken'
 import {Request, Response, NextFunction} from 'express'
-import User from '../models/User';
-import { IUser } from '../utils/types';
 import { throwError } from '../utils/functions';
 
 export let authenticate = async (req:Request, res:Response, next:NextFunction) => {
@@ -11,11 +9,7 @@ export let authenticate = async (req:Request, res:Response, next:NextFunction) =
         }
         let token:string = req.header('Authorization')!.replace('Bearer ', '');
         console.log(token)
-        const decode = verify(token, process.env.ACCESS_TOKEN_SECRET!) as {id: string}
-        const user: IUser = await User.findById(decode.id)
-        if (!user) {
-            throw new Error()
-        }
+        verify(token, process.env.ACCESS_TOKEN_SECRET!) as {id: string}
         next();
     } catch (error) {
         throwError(res, 403, "USER NOT AUTHENTICATED")
