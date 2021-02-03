53c7a549060606923598efdfb3dfeaf2bdb30dd0
Lagoons Browning <lagoons@protonmail.com>
2021-02-03 17:19:18 -0600

    access control origin can die

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,5 @@
 echo What should the version be?
 read VERSION
-echo "Commit message?"
-read COMMIT
-
-git add .
-git commit -am "$COMMIT"
-git push origin master
-
-sleep 420 
 
 ssh root@149.28.120.33 "
 docker pull infecting/indiefilms:latest
